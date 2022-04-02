7177f97fd4779734a46c36b09717b76401ac58a1
Stefy Sabbadin <9255011+sabbakix@users.noreply.github.com>
2022-04-02 15:45:53 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -3,30 +3,6 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  /* create message variable */
-  const [message, setMessage] = useState("");
-  const people = [
-    { name: "Stefy", age: 39, pets: [{ name: "mimi" }, { name: "tony" }] },
-    { name: "Lor", age: 65, pets: [{ name: "youyou" }, { name: "maxi" }] }
-  ];
-
   //jsx
-  return (
-    <div className="App">
-      <h1>Test</h1>
-      {/** show message */}
-      <p>{message}</p>
-
-      <MyComponent />
-
-      {people.map((person) => (
-        <div key={person.name}>
-          <h2>{person.name}</h2>
-          {person.pets.map((pet) => (
-            <span>{pet.name} &nbsp;</span>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  return <></>;
 }
